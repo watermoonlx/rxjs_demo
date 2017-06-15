70495c71dea167a63262e2707de07c51745d9aaa
Jake.C.Xiao <Jake.C.Xiao@newegg.com>
2017-06-15 23:43:23 +0800

    temp save

--- src/4.observer/app.ts
+++ src/4.observer/app.ts
@@ -1,5 +1,11 @@
+import * as Rx from 'rxjs';
+
 let observer = {
     next: (x:any) => console.log('Observer got a next value: ' + x),
     error: (err:any) => console.error('Observer got an error: ' + err),
     complete: () => console.log('Observer got a complete notification'),
 };
+
+let a$ = Rx.Observable.of(1);
+
+a$.subscribe(observer);
