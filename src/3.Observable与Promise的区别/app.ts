9af5004f5ea3e5ea2a44cc6a9b97d28412078edc
Jake.C.Xiao <Jake.C.Xiao@newegg.com>
2017-06-15 22:40:42 +0800

    temp save

--- src/3.Observable与Promise的区别/app.ts
+++ src/3.Observable与Promise的区别/app.ts
@@ -41,3 +41,18 @@ let subscription2 = observable.subscribe(i => {
 // });
 
 
+//同步性
+// let a$ = Rx.Observable.create((observer: any) => {
+//     console.log('Start!');
+//     observer.next(123);
+//     console.log('Done!');
+//     observer.complete();
+// });
+
+// console.log('订阅前');
+// a$.subscribe((i: any) => {
+//     console.log(`收到的值是 ${i}`);
+// });
+// console.log('订阅后');
+
+
